ef9186bd3897c101ef31389ee78710b312d14489
Thony0726 <richardanto0726@gmail.com>
2022-03-03 23:54:48 -0500

    usuario1

--- app/src/main/java/com/example/appventas/activity_registro.java
+++ app/src/main/java/com/example/appventas/activity_registro.java
@@ -21,6 +21,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class activity_registro extends AppCompatActivity {
@@ -31,6 +36,8 @@ public class activity_registro extends AppCompatActivity {
     private Button btnRegistrar;
     private CheckBox checkBox;
     FirebaseAuth auth;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +52,7 @@ public class activity_registro extends AppCompatActivity {
         etPassword = (EditText) findViewById(R.id.etPassword);
         btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
         checkBox = (CheckBox) findViewById(R.id.checkBox);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -62,6 +70,7 @@ public class activity_registro extends AppCompatActivity {
             public void onClick(View view) {
                 String userE = etUser.getText().toString();
                 String passE = etPassword.getText().toString();
+
                 //Ahora validamos por si uno de los campos esta vacío
                 if (TextUtils.isEmpty(userE)) {
                     //por si falta correo
@@ -73,10 +82,8 @@ public class activity_registro extends AppCompatActivity {
                     Toast.makeText(activity_registro.this, "Inserte contraseña", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
                 progressDialog.setMessage("En proceso");
                 progressDialog.show();
-
                 //Ahora usamos el metodo
                 auth.createUserWithEmailAndPassword(userE, passE).
                         //Le pasamos la clase registro
@@ -85,21 +92,28 @@ public class activity_registro extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 //Task=Tareas devuelve si la tarea si se cumple
                                 //En este caso si se cumplio
-                                Toast.makeText(activity_registro.this, "Usuario registrado exitosamente", Toast.LENGTH_SHORT).show();
                                 //Si no logra registrarse
                                 if (!task.isSuccessful()) {
                                     Toast.makeText(activity_registro.this, "Usuario no se ha podido registrar", Toast.LENGTH_SHORT).show();
                                     return;
                                 }
+                                /*=======================*/
+                                Map<String, Object> personaMap = new HashMap<>();
+                                personaMap.put("usuUsuario", "" + etUser.getText().toString());
+                                personaMap.put("usuClave", "" + etPassword.getText().toString());
+                                mDatabase.child("Usuarios").push().setValue(personaMap);
+                                Toast.makeText(activity_registro.this, "Usuario registrado exitosamente", Toast.LENGTH_SHORT).show();
+                                /*=======================*/
                                 Intent i = new Intent(activity_registro.this, activity_persona.class);
-                                i.putExtra("correo",etUser.getText().toString());
+                                i.putExtra("correo", etUser.getText().toString());
                                 startActivity(i);
                                 finish();
+
                             }
                         });
+
             }
         });
-
     }
 
 
